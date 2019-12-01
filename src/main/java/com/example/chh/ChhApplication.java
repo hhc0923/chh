67dbd60d4970abb5c345c3dd149ba83eb54f9a9f
jiangll1 <jiangll1@ziroom.com>
2019-12-01 21:52:09 +0800

    'first'

---
+++ src/main/java/com/example/chh/ChhApplication.java
@@ -0,0 +1,13 @@
+package com.example.chh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chh/ChhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
